ca22484f241593edf397b42469498feb465dbd3a
magnestik <magnestik@rambler.ru>
2022-02-18 15:41:49 +0300

    Initial commit

---
+++ src/main/java/ru/iteco/teachbase/springjunior/SpringJuniorApplication.java
@@ -0,0 +1,13 @@
+package ru.iteco.teachbase.springjunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJuniorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJuniorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/iteco/teachbase/springjunior/SpringJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iteco.teachbase.springjunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJuniorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
