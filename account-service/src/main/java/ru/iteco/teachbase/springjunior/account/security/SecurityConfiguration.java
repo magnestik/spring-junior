0c2bd73096a60821f7df890b3f5f11e06a67f424
Zaur <zaur_v@bk.ru>
2022-08-02 11:51:58 +0300

    part08_01

--- account-service/src/main/java/ru/iteco/teachbase/springjunior/account/security/SecurityConfiguration.java
+++ account-service/src/main/java/ru/iteco/teachbase/springjunior/account/security/SecurityConfiguration.java
@@ -2,34 +2,32 @@ package ru.iteco.teachbase.springjunior.account.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    private final DbUserDetailService dbUserDetailService;
-
-    public SecurityConfiguration(DbUserDetailService dbUserDetailService) {
-        this.dbUserDetailService = dbUserDetailService;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(dbUserDetailService);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        return http
             .csrf().disable()
             .authorizeRequests().antMatchers("/rest/user/**").hasAuthority("admin")
             .and().authorizeRequests().antMatchers("/auth/create").anonymous()
             .and().authorizeRequests().anyRequest().authenticated()
             .and().httpBasic()
-            .and().sessionManagement().disable();
+            .and().sessionManagement().disable()
+            .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
